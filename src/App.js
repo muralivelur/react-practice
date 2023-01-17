c609a7f4054dc8a7aa2ed82f41f76213c69b5572
muralivelur <96679601+muralivelur@users.noreply.github.com>
2023-01-17 23:46:50 +0530

    2 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from "react";
-import "./style.css";
+import React, { Component, Fragment } from 'react';
+import { Greeting } from './components/Greeting';
+import './style.css';
 
-export default function App() {
+//class App extends React.Component {}
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Welcome Murali Bro !!!</h1>
+        <Greeting name="sathwik" />
+        <Greeting name="my dear !!!" />
+      </Fragment>
+    );
+  }
+}
+
+/*export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
-}
+}*/
+
+export default App;
---
+++ src/components/Greeting.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// class component/*
+/*class Greeting extends Component {
+  render() {
+    return <h2> Good Morning {this.props.name}</h2>;
+  }
+}*/
+
+function Greeting(props) {
+  return <h2> Good Morning {props.name}</h2>;
+}
+
+export { Greeting };
