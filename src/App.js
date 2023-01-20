b22a8979c9c3713d11dcfcd1fa869447b0694cab
muralivelur <96679601+muralivelur@users.noreply.github.com>
2023-01-20 13:54:21 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import { person1, person2 } from './person';
 import './style.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = { name: 'Falabella' };
+    // bind the context this object to the method
+    this.updateName = this.changeName.bind(this);
+  }
+  changeName() {
+    this.setState({ name: 'Sodimac' });
+  }
   render() {
     return (
       <main>
-        <h1> Members </h1>
+        <h1> Members {this.state.name}</h1>
         <UserInfo {...person1} />
         <UserInfo {...person2} />
+        <button onClick={this.updateName}> Change Me</button>
       </main>
     );
   }
