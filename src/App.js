233ab70f43ef352efb51c18f272141eb8cec7c57
muralivelur <96679601+muralivelur@users.noreply.github.com>
2023-01-21 15:34:56 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import { Greeting } from './components/Greeting';
 import './style.css';
 
 class App extends Component {
-  constructor() {
+  /* constructor() {
     super();
     this.state = { name: 'Falabella' };
-  }
+    // bind method can be avoided beacuse of the arrow functions defined at changeState
+  }*/
 
+  //constructor also can be removed and keep the state defined out of the constructor
+
+  state = { name: 'Falabella' };
   changeState = () => {
     this.setState({ name: 'Sodimac' });
   };
 
+  componentWillMount() {
+    console.log("I'm about to mounted");
+  }
+  componentDidMount() {
+    console.log("I'm just mount");
+  }
   render() {
     return (
       <main>
