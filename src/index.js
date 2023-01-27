a370e4f085c37bdf0ca5722bfe351d89cf9df484
muralivelur <96679601+muralivelur@users.noreply.github.com>
2023-01-27 08:16:27 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { StrictMode } from 'react';
+/*import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
@@ -10,4 +10,36 @@ root.render(
   <StrictMode>
     <App />
   </StrictMode>
-);
+);*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import {createRoot} from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+//const root = createRoot(document.getElementById('root'));
+
+/*function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  root.render(element);
+}*/
+
+function Clock(props) {
+  return (
+    <div>
+      <h1> Hello, World </h1>
+      <h2> it is {props.date.toLocaleTimeString()} </h2>
+    </div>
+  );
+}
+
+function tick() {
+  root.render(<Clock date={new Date()} />);
+}
+
+setInterval(tick, 1000);
